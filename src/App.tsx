c39f7bcad34e7fa3113ac6f36460220ed176b55d
bhupinder <bhupindersingh987686@gmail.com>
2023-01-05 16:07:25 +0530

    changes in app component

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App (): JSX.Element {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React
+            Reacts
           </a>
           <span>, </span>
           <a
