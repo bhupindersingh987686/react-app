47e6638b15df4844d7cea5875510eb40b9137849
bhupinder <bhupindersingh987686@gmail.com>
2023-01-05 15:16:41 +0530

    init react project

---
+++ src/features/counter/counterAPI.ts
@@ -0,0 +1,7 @@
+// A mock function to mimic making an async request for data
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function fetchCount (amount = 1) {
+  return await new Promise<{ data: number }>((resolve) =>
+    setTimeout(() => { resolve({ data: amount }) }, 500)
+  )
+}
